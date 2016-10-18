2efee7d9b4217ff95214e67bd57a94b446663055
Rohit Raghavan <rohit.raghavan@berkeley.edu>
2016-10-18 13:09:34 -0700

    MongoDB Lab

---
+++ Labs/lab-SQL/lab-SQL/Lab-SQLite/app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, redirect, request
+from app import app, models, db
+from .forms import CustomerForm
+# Access the models file to use SQL functions
+
+
+@app.route('/')
+def index():
+    return redirect('/create_customer')
+
+
+@app.route('/create_customer', methods=['GET', 'POST'])
+def create_customer():
+    form = CustomerForm()
+    if form.validate_on_submit():
+        # Get data from the form
+        # Send data from form to Database
+        company = form.company.data
+        email = form.email.data
+        models.insert_customer(company, email)
+        return redirect('/customers')
+    return render_template('customer.html', form=form)
+
+
+@app.route('/customers')
+def display_customer():
+    # Retreive data from database to display
+    customers = models.retrieve_customers()
+    return render_template('home.html',
+                           customers=customers)
