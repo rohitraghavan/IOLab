971a656084bc088a3dde88e1d2765be306039732
Rohit Raghavan <rohit.raghavan@berkeley.edu>
2016-10-18 13:09:34 -0700

    MongoDB Lab

---
+++ Labs/Lab11-ORMS/orm_tutorial/app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, redirect, request
+from app import app, models, db
+from .forms import CustomerForm
+
+
+@app.route('/')
+def index():
+    return redirect('/create_customer')
+
+
+@app.route('/create_customer', methods=['GET', 'POST'])
+def create_customer():
+    form = CustomerForm()
+    # Capture form data and send to database
+    if form.validate_on_submit():
+        customer = models.Customer(
+            company=form.company.data,
+            email=form.email.data)
+        db.session.add(customer)
+        db.session.commit()
+        return redirect('/customers')
+    return render_template('customer.html', form=form)
+
+
+@app.route('/customers')
+def display_customer():
+    # Fetch customer data from database
+    customers = models.Customer.query.all()
+    return render_template('home.html',
+                           customers=customers)
--- Labs/MongoDB_Lab/lab_mongo/app/model.py
+++ Labs/MongoDB_Lab/lab_mongo/app/model.py
@@ -1,10 +1,25 @@
 import pymongo
 
 
-#gets you the handler on the mongo client
+# gets you the handler on the mongo client
+client = pymongo.MongoClient()
+# choose the data base
+db = client.Surveys
+# choose the collection
+collection = db.usersurveys
+# example code
 
-#choose the data base
 
-#choose the collection
+def insertRecords(username, email, surveyResponse):
+    collection.insert({"username": username,
+                       "email": email,
+                       "color": surveyResponse['color'],
+                       "food": surveyResponse['food'],
+                       "vacation": surveyResponse['vacation'],
+                       "fe-before": surveyResponse['fe-before'],
+                       "fe-after": surveyResponse['fe-after']
+                       })
 
-#example code
+
+def display():
+    return collection
